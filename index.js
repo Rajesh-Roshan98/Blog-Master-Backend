16d462f5d9e77835cdb28559fee41820bc1d214a
Rajesh-Roshan98 <rrajeshroshan89@gmail.com>
2025-07-04 21:44:51 +0530

    Backend is Completed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:3000'], 
+  origin: ['http://localhost:5173', 'http://localhost:3000'], 
   credentials: true
 }));
 
