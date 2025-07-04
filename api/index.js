c1535f32db7b8d74f90b4818092b6df57bd22974
Rajesh-Roshan98 <rrajeshroshan89@gmail.com>
2025-07-05 01:13:06 +0530

    Backend is Completed

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
+// Root route for health check or friendly message
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
+
+// Handle favicon.ico requests with a 204 No Content
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 const express = require('express');
 require('dotenv').config();
 const { dbConnect } = require('../config/dbConnect');
